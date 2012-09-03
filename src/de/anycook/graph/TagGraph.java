c3f66893f59ac1e126d0cbfbe971cdef0cf9ecc0
Jan Graßegger <jan@anycook.de>
2012-09-03 10:55:35 +0200

    added

--- src/de/anycook/graph/TagGraph.java
+++ src/de/anycook/graph/TagGraph.java
@@ -1,6 +1,7 @@
 package de.anycook.graph;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,6 +14,7 @@ import javax.ws.rs.core.Response;
 import org.json.simple.JSONObject;
 
 import de.anycook.misc.JsonpBuilder;
+import de.anycook.recipe.Recipe;
 import de.anycook.tag.Tag;
 
 
@@ -40,6 +42,26 @@ public class TagGraph {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getNum(@QueryParam("callback") String callback){
 		return JsonpBuilder.buildResponse(callback, Tag.getTotal());
+	}
+	
+	/**
+	 * Tags ordered by popularity.
+	 * @param recipe If set tags of this recipe are excluded
+	 * @param callback 
+	 * @return Map of tags ordered by popularity with number of recipes
+	 */
+	@GET
+	@Path("popular")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getPopularTags(@QueryParam("recipe") String recipe,
+			@QueryParam("callback") String callback){
+			Map<String, Integer> tagmap = null;
+		if(recipe==null)
+				 tagmap = Recipe.getPopularTags();		
+		else
+			tagmap = Recipe.getPopularTags(recipe);	
+		return JsonpBuilder.buildResponse(callback, tagmap);
+		
 	}
 	
 	@GET
