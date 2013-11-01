e2829c7bdaa14d2197678d31bd7b9faeacd7f6b2
Jan Graßegger <krankgesund@googlemail.com>
2013-11-02 00:14:25 +0100

    LifeGraph is now async

--- src/main/java/de/anycook/api/LifeGraph.java
+++ src/main/java/de/anycook/api/LifeGraph.java
@@ -5,14 +5,19 @@ package de.anycook.api;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import javax.ws.rs.container.TimeoutHandler;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import de.anycook.news.life.Life;
 import de.anycook.news.life.LifeHandler;
 import org.apache.log4j.Logger;
+import org.glassfish.jersey.server.ManagedAsync;
 
 /**
  * Graph for lifes stream
@@ -24,18 +29,32 @@ public class LifeGraph {
     private final Logger logger = Logger.getLogger(getClass());
 
 	@GET
+    @ManagedAsync
 	@Produces(MediaType.APPLICATION_JSON+";charset=UTF-8")
-	public List<Life> getLifes(@QueryParam("newestid") Integer newestid,
-			@QueryParam("oldestid") Integer oldestid){
+	public void getLives(@Suspended AsyncResponse asyncResponse, @QueryParam("newestid") Integer newestId,
+                         @QueryParam("oldestid") Integer oldestId){
 
-        try {
-            if(newestid != null)
-                return LifeHandler.getLastLives(newestid);
-            else
-                return LifeHandler.getOlderLives(oldestid);
-        } catch (SQLException e){
-            logger.error(e);
-            throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
+        asyncResponse.setTimeoutHandler(new TimeoutHandler() {
+            @Override
+            public void handleTimeout(AsyncResponse asyncResponse) {
+                logger.info("reached timeout");
+                asyncResponse.resume(Response.ok().build());
+            }
+        });
+
+        asyncResponse.setTimeout(5, TimeUnit.MINUTES);
+
+        try{
+            while(asyncResponse.isSuspended() && !asyncResponse.isCancelled() && !asyncResponse.isDone()){
+                List<Life> lives = newestId != null ?
+                        LifeHandler.getLastLives(newestId) : LifeHandler.getOlderLives(oldestId);
+
+                if(lives.size() > 0) asyncResponse.resume(lives);
+                else Thread.sleep(1000);
+            }
+        } catch (SQLException | InterruptedException e){
+            logger.error(e,e);
+            asyncResponse.resume(new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR));
         }
 
 	}
