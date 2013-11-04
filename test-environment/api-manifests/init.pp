d67755abb9182426ffb419b3cd13f2ddf1b5747d
Jan Graßegger <krankgesund@googlemail.com>
2013-11-04 14:11:20 +0100

    added mongodb to test-environment

--- test-environment/api-manifests/init.pp
+++ test-environment/api-manifests/init.pp
@@ -187,10 +187,12 @@ class sass {
 
 class mongodb {
   apt::source { 'mongodb':
-    location   => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart',
-    repos      => 'dist 10gen',
+    location   => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart/',
+    release    => 'dist',
+    repos      => ' 10gen',
     key        => '7F0CEB10',
     key_server => 'keyserver.ubuntu.com',
+    include_src       => false,
   }
 
   package { "mongodb-10gen":
