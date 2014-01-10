f983fe5c2ef90521c5202e9d0992968f659923b4
Jan Graßegger <krankgesund@googlemail.com>
2014-01-10 01:58:02 +0100

    added ingredient method

--- web/js/jquery.anycook.api.ingredient.js
+++ web/js/jquery.anycook.api.ingredient.js
@@ -2,8 +2,39 @@
  * @author Jan Graßegger <jan@anycook.de>
  */
 
-$.anycook.api.ingredient = function(callback){
+//ingredient([ingredientName or parent] [callback])
+$.anycook.api.ingredient = function(){
+	var ingredientName;
+	var parent;
+	var callback;
+	switch(arguments.length){
+
+	case 2:
+		var type2 = typeof arguments[1];
+		if(type2 == "function")
+			callback = arguments[1];
+	
+	case 1:
+		var type1 = typeof arguments[0];
+		if(type1 == "string")
+			ingredientName = arguments[0];
+		else if(type1 == "function")
+			callback = arguments[0];
+		else if(type1 == "boolean")
+			parent = arguments[0];	
+	}
+	
+	
+
 	var path = "/ingredient";
+	if(ingredientName)
+		path+="/"+encodeURIComponent(ingredientName);
+
+	var data = {};
+
+	if(parent)
+		$.extend(data, {parent : parent});
+
 	return $.anycook.api._get(path, {}, callback);
 }
 
