9939c4e7ea962915710ef0b7234b4050ac8ab0c8
Jan Graßegger <jan@mbr-targeting.com>
2016-12-05 19:34:42 +0100

    Refactored recipe classes.

--- src/main/java/de/anycook/db/mysql/DBSaveRecipe.java
+++ src/main/java/de/anycook/db/mysql/DBSaveRecipe.java
@@ -21,6 +21,7 @@ package de.anycook.db.mysql;
 import de.anycook.newrecipe.NewRecipe;
 import de.anycook.recipe.ingredient.Ingredient;
 import de.anycook.recipe.step.Step;
+
 import org.apache.lucene.queryparser.classic.ParseException;
 
 import java.io.IOException;
@@ -38,16 +39,19 @@ public class DBSaveRecipe extends DBRecipe {
         super();
     }
 
-    public void newVersion(int id, NewRecipe newRecipe) throws ParseException, SQLException, IOException {
+    public void newVersion(final int id, final NewRecipe newRecipe)
+            throws ParseException, SQLException, IOException {
         newVersion(id, newRecipe, -1);
     }
 
     // new recipe and new version
-    public void newVersion(int id, NewRecipe newRecipe, int userId) throws IOException, ParseException, SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO versions(id, gerichte_name, " +
-                "eingefuegt, users_id, beschreibung, skill, kalorien, imagename, std, min, personen, " +
-                "kategorien_name,comment) VALUES(?,?, NOW(), ?, ?, ?, ?, ? , ? ,? ,?, ?,?)");
-
+    public void newVersion(final int id, final NewRecipe newRecipe, final int userId)
+            throws IOException, ParseException, SQLException {
+        final PreparedStatement pStatement = connection.prepareStatement(
+                "INSERT INTO versions(id, gerichte_name, "
+                + "eingefuegt, users_id, beschreibung, skill, kalorien, "
+                + "imagename, std, min, personen, kategorien_name,comment) "
+                + "VALUES(?,?, NOW(), ?, ?, ?, ?, ? , ? ,? ,?, ?,?)");
 
         pStatement.setInt(1, id);
         pStatement.setString(2, newRecipe.name);
@@ -68,27 +72,31 @@ public class DBSaveRecipe extends DBRecipe {
         logger.info("newversion for {} added from {}", newRecipe.name, userId);
     }
 
-    public int getLastId(String recipeName) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("SELECT id from versions WHERE gerichte_name = ? " +
-                "ORDER BY id DESC LIMIT 1");
+    public int getLastId(final String recipeName) throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("SELECT id FROM versions WHERE gerichte_name = ? "
+                                            + "ORDER BY id DESC LIMIT 1");
 
         pStatement.setString(1, recipeName);
-        try (ResultSet data = pStatement.executeQuery()) {
-            if (data.next())
+        try (final ResultSet data = pStatement.executeQuery()) {
+            if (data.next()) {
                 return data.getInt("id");
+            }
             return 0;
         }
     }
 
-    public void newRecipe(String name) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO gerichte(name, eingefuegt) " +
-                "VALUES (?, NOW())");
+    public void newRecipe(final String name) throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("INSERT INTO gerichte(name, eingefuegt) "
+                                            + "VALUES (?, NOW())");
         pStatement.setString(1, name);
         pStatement.executeUpdate();
     }
 
-    private void addSteps(String recipeName, int versionId, List<Step> steps) throws SQLException {
-        for (Step step : steps) {
+    private void addSteps(final String recipeName, final int versionId,
+                          final List<Step> steps) throws SQLException {
+        for (final Step step : steps) {
             addStep(recipeName, step.getId(), versionId, step.getText(), step.getIngredients());
         }
     }
@@ -96,14 +104,15 @@ public class DBSaveRecipe extends DBRecipe {
     /**
      * Erstellt neuen Schritt zu einem Gericht
      *
-     * @param description
-     * @param ingredients
      * @return bei erfolg true, sonst false
      */
-    private void addStep(String recipeName, int stepId, int versionId, String description,
-                                      List<Ingredient> ingredients) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO schritte(idschritte, versions_id, " +
-                "versions_gerichte_name, beschreibung) VALUES (?,?,?,?)");
+    private void addStep(final String recipeName, final int stepId, final int versionId,
+                         final String description, final List<Ingredient> ingredients)
+            throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("INSERT INTO schritte(idschritte, versions_id, "
+                                            + "versions_gerichte_name, beschreibung) "
+                                            + "VALUES (?,?,?,?)");
 
         pStatement.setInt(1, stepId);
         pStatement.setInt(2, versionId);
@@ -114,26 +123,29 @@ public class DBSaveRecipe extends DBRecipe {
         addStepIngredients(recipeName, stepId, versionId, ingredients);
     }
 
-    private void addStepIngredients(String recipeName, int stepId,
-                                    int versionId, List<Ingredient> ingredients) throws SQLException {
+    private void addStepIngredients(final String recipeName, final int stepId,
+                                    final int versionId, final List<Ingredient> ingredients)
+            throws SQLException {
 
         int position = 1;
 
-        for (Ingredient ing : ingredients) {
+        for (final Ingredient ing : ingredients) {
             addStepIngredient(recipeName, stepId, versionId, ing, position++);
         }
 
     }
 
-    private void addStepIngredient(String recipeName, int stepId, int versionId, Ingredient ingredient, int i)
+    private void addStepIngredient(final String recipeName, final int stepId, final int versionId,
+                                   final Ingredient ingredient, final int i)
             throws SQLException {
         if (ingredient.getName().isEmpty()) {
             return;
         }
 
-        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO " +
-                "schritte_has_zutaten(schritte_idschritte, schritte_versions_id, schritte_versions_gerichte_name, " +
-                "zutaten_name, menge, position) VALUES (?,?,?,?, ?,?)");
+        final PreparedStatement pStatement = connection.prepareStatement(
+                "INSERT INTO schritte_has_zutaten(schritte_idschritte, schritte_versions_id, "
+                + "schritte_versions_gerichte_name, zutaten_name, menge, position) "
+                + "VALUES (?,?,?,?, ?,?)");
 
         pStatement.setInt(1, stepId);
         pStatement.setInt(2, versionId);
@@ -144,7 +156,8 @@ public class DBSaveRecipe extends DBRecipe {
         pStatement.executeUpdate();
     }
 
-    private void addIngredients(String recipeName, int versionId, List<Ingredient> ingredients)
+    private void addIngredients(final String recipeName, final int versionId,
+                                final List<Ingredient> ingredients)
             throws SQLException, IOException, ParseException {
         for (int i = 0; i < ingredients.size(); i++) {
             addIngredient(recipeName, versionId, ingredients.get(i), i);
@@ -153,19 +166,19 @@ public class DBSaveRecipe extends DBRecipe {
 
     /**
      * Fuegt Zutat zu bestehendem Gericht hinzu
-     *
-     * @throws java.sql.SQLException
      */
     public void addIngredient(String recipeName, int versionId, Ingredient ingredient, int position)
             throws SQLException, IOException, ParseException {
 
-
         try (DBIngredient dbIngredient = new DBIngredient()) {
-            if (!dbIngredient.exists(ingredient.getName())) dbIngredient.newIngredient(ingredient.getName());
+            if (!dbIngredient.exists(ingredient.getName())) {
+                dbIngredient.newIngredient(ingredient.getName());
+            }
         }
 
-        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO versions_has_zutaten" +
-                "(versions_gerichte_name, versions_id, zutaten_name, menge, position) VALUES (?,?,?,?,?)");
+        final PreparedStatement pStatement = connection.prepareStatement(
+                "INSERT INTO versions_has_zutaten(versions_gerichte_name, versions_id, "
+                + "zutaten_name, menge, position) VALUES (?,?,?,?,?)");
 
         pStatement.setString(1, recipeName);
         pStatement.setInt(2, versionId);
@@ -173,11 +186,13 @@ public class DBSaveRecipe extends DBRecipe {
         pStatement.setString(4, ingredient.getAmount());
         pStatement.setInt(5, position);
         pStatement.executeUpdate();
-        logger.info("added ingredient '" + ingredient.getName() + "' to " + recipeName);
+        logger.info("added ingredient '{}' to {}", ingredient.getName(), recipeName);
     }
 
     public void addTags(String recipeName, Set<String> tags, int userId) throws SQLException {
-        for (String tag : tags) addTag(recipeName, tag, userId);
+        for (final String tag : tags) {
+            addTag(recipeName, tag, userId);
+        }
     }
 
     /**
@@ -186,64 +201,78 @@ public class DBSaveRecipe extends DBRecipe {
      * @param recipe Name des Gerichts
      * @param tag    Name des Tags
      */
-    public void addTag(String recipe, String tag, int userId) throws SQLException {
-        try (DBTag dbTag = new DBTag()) {
-            if (!dbTag.exists(tag)) dbTag.create(tag);
+    public void addTag(final String recipe, final String tag, final int userId)
+            throws SQLException {
+        try (final DBTag dbTag = new DBTag()) {
+            if (!dbTag.exists(tag)) {
+                dbTag.create(tag);
+            }
         }
 
-        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO gerichte_has_tags(gerichte_name, " +
-                "tags_name, active, users_id) VALUES (?,?, 1, ?)");
+        final PreparedStatement pStatement =
+                connection.prepareStatement("INSERT INTO gerichte_has_tags(gerichte_name, "
+                                            + "tags_name, active, users_id) VALUES (?,?, 1, ?)");
         pStatement.setString(1, recipe);
         pStatement.setString(2, tag);
         pStatement.setInt(3, userId);
         pStatement.executeUpdate();
-        logger.info("added tag '" + tag + "' to " + recipe);
+        logger.info("added tag '{}' to {}", tag, recipe);
     }
 
-    public boolean isTasty(String recipeName, int userId) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("SELECT * FROM schmeckt WHERE users_id = ? " +
-                "AND gerichte_name = ?");
+    public boolean isTasty(final String recipeName, final int userId) throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("SELECT * FROM schmeckt WHERE users_id = ? "
+                                            + "AND gerichte_name = ?");
         pStatement.setInt(1, userId);
         pStatement.setString(2, recipeName);
         ResultSet data = pStatement.executeQuery();
         return data.next();
     }
 
-    public void makeTasty(String recipeName, int userId) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO schmeckt(gerichte_name, users_id, eingefuegt) VALUES (?,?, NOW())");
+    public void makeTasty(final String recipeName, final int userId) throws SQLException {
+        final PreparedStatement pStatement = connection.prepareStatement(
+                "INSERT INTO schmeckt(gerichte_name, users_id, eingefuegt) "
+                + "VALUES (?,?, NOW())");
         pStatement.setString(1, recipeName);
         pStatement.setInt(2, userId);
         pStatement.executeUpdate();
     }
 
-    public void unmakeTasty(String recipeName, int userId) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("DELETE FROM schmeckt WHERE gerichte_name = ? " +
-                "AND users_id = ?");
+    public void unmakeTasty(final String recipeName, final int userId) throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("DELETE FROM schmeckt WHERE gerichte_name = ? "
+                                            + "AND users_id = ?");
         pStatement.setString(1, recipeName);
         pStatement.setInt(2, userId);
         pStatement.executeUpdate();
     }
 
-    public void unmakeTasty(int userId) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("DELETE FROM schmeckt WHERE users_id = ?");
+    public void unmakeTasty(final int userId) throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("DELETE FROM schmeckt WHERE users_id = ?");
         pStatement.setInt(1, userId);
         pStatement.executeUpdate();
     }
 
-    public void suggestTag(String recipeName, String tag, int userId) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("INSERT INTO gerichte_has_tags (gerichte_name, " +
-                "tags_name, users_id) VALUES (?,?,?)");
+    public void suggestTag(final String recipeName, final String tag,
+                           final int userId) throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("INSERT INTO gerichte_has_tags (gerichte_name, "
+                                            + "tags_name, users_id) VALUES (?,?,?)");
 
         pStatement.setString(1, recipeName);
         pStatement.setString(2, tag);
         pStatement.setInt(3, userId);
         pStatement.executeUpdate();
-        logger.info(userId + " suggested new tag \"" + tag + "\" for " + recipeName);
+        logger.info(userId + " suggested new tag '{}' for {}", tag, recipeName);
     }
 
-    public void changeImageName(String recipeName, int versionId, String newImageName) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("UPDATE versions SET imagename = ? " +
-                "WHERE gerichte_name = ? AND id = ?");
+    public void changeImageName(final String recipeName, final int versionId,
+                                final String newImageName)
+            throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("UPDATE versions SET imagename = ? " +
+                                            "WHERE gerichte_name = ? AND id = ?");
 
         pStatement.setString(1, newImageName);
         pStatement.setString(2, recipeName);
@@ -252,10 +281,13 @@ public class DBSaveRecipe extends DBRecipe {
         pStatement.executeUpdate();
     }
 
-    public void changeStep(String recipeName, int versionId, int stepId, String newText) throws SQLException {
+    public void changeStep(final String recipeName, final int versionId, final int stepId,
+                           final String newText)
+            throws SQLException {
 
-        PreparedStatement pStatement = connection.prepareStatement("UPDATE schritte SET beschreibung = ? " +
-                "WHERE versions_gerichte_name = ? AND versions_id = ? AND idschritte = ?");
+        final PreparedStatement pStatement = connection.prepareStatement(
+                "UPDATE schritte SET beschreibung = ? "
+                + "WHERE versions_gerichte_name = ? AND versions_id = ? AND idschritte = ?");
 
         pStatement.setString(1, newText);
         pStatement.setString(2, recipeName);
@@ -266,32 +298,34 @@ public class DBSaveRecipe extends DBRecipe {
 
     }
 
-    public void deleteIngredient(String zutat) throws SQLException {
-        PreparedStatement pStatement = connection.prepareStatement("DELETE FROM zutaten WHERE name = ?");
+    public void deleteIngredient(final String zutat) throws SQLException {
+        final PreparedStatement pStatement =
+                connection.prepareStatement("DELETE FROM zutaten WHERE name = ?");
 
         pStatement.setString(1, zutat);
         pStatement.executeUpdate();
     }
 
 
-    public void setActiveId(String recipeName, int activeId) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE gerichte SET active_id = ? WHERE name = ?");
+    public void setActiveId(final String recipeName, final int activeId) throws SQLException {
+        final PreparedStatement preparedStatement =
+                connection.prepareStatement("UPDATE gerichte SET active_id = ? WHERE name = ?");
         preparedStatement.setInt(1, activeId);
         preparedStatement.setString(2, recipeName);
         preparedStatement.executeUpdate();
     }
 
-    public void increaseViewCount(String recipeName) throws SQLException {
-        PreparedStatement preparedStatement =
-                connection.prepareStatement("UPDATE gerichte SET viewed = viewed +1 WHERE name = ?");
+    public void increaseViewCount(final String recipeName) throws SQLException {
+        final PreparedStatement preparedStatement = connection.prepareStatement(
+                "UPDATE gerichte SET viewed = viewed +1 WHERE name = ?");
         preparedStatement.setString(1, recipeName);
         preparedStatement.executeUpdate();
     }
 
-    public void setLastChange(String recipeName) throws SQLException {
-        long currentTime = System.currentTimeMillis();
-        PreparedStatement preparedStatement =
-            connection.prepareStatement("UPDATE gerichte SET last_change = ? WHERE name = ?");
+    public void setLastChange(final String recipeName) throws SQLException {
+        final long currentTime = System.currentTimeMillis();
+        final PreparedStatement preparedStatement = connection.prepareStatement(
+                "UPDATE gerichte SET last_change = ? WHERE name = ?");
         preparedStatement.setTimestamp(1, new Timestamp(currentTime));
         preparedStatement.setString(2, recipeName);
         preparedStatement.executeUpdate();
